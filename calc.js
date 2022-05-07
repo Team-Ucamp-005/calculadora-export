62c4739fa402f287cb18485086ace08c83a10207
Angel25Novoa <jorge.250495@gmail.com>
2022-05-07 11:05:03 -0500

    calculadora/import/export

---
+++ calc.js
@@ -0,0 +1,9 @@
+// const suma = require('./suma');
+// const resta = require('./resta');
+// const mult = require('./mult');
+// const div = require('./div');
+import suma from './suma.js';
+suma(5,16)
+// resta(5,16)
+// mult(5,16)
+// div(5,16)
---
+++ suma.js
@@ -0,0 +1,9 @@
+const suma = (a, b) =>{
+  console.log(a + b);
+  return a + b;
+}
+
+export default suma;
+
+// module.exports = suma;
+
